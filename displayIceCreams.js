1302d91fbfb7a1a5f95d8bc2a92c6a47b2e494fb
Iago <iagomolinavazquez@gmail.com>
2023-06-19 15:29:32 -0300

    Optimize list generation
     Changes to be committed:
    	modified:   displayIceCreams.js

--- displayIceCreams.js
+++ displayIceCreams.js
@@ -2,26 +2,27 @@ function createNode(elementType, styleClass, text) {
     let node = document.createElement(elementType.toUpperCase());
     node.classList.add(styleClass);
     if (text != null) {
-        node.textContent = text;
+        node.innerHTML = text;
     }
     return node;
 }
 
-function addRow(styleClass, leftText, rightText) {
-    let list = document.getElementById("main__list");
+function getRow(styleClass, leftText, rightText) {
     let listRow = createNode("div", styleClass);
     listRow.appendChild(createNode("div", "list-item", leftText));
     listRow.appendChild(createNode("div", "list-item", rightText));
-    list.appendChild(listRow);
+    return listRow;
 }
 
 const iceCreams = [new IceCream("Banana Split", 600),   new IceCream("Chocolate", 500),
                    new IceCream("Strawberry",   500),   new IceCream("Cream",     400),
                    new IceCream("Lemon",        300)];
 
-addRow("list-element-description", "Ice Cream", "Price");
-
+let list = document.getElementById("main__list");
+let listElements = document.createDocumentFragment();
+listElements.appendChild(getRow("list-element-description", "Ice Cream", "Price"));
 
 iceCreams.forEach(iceCream => {
-    addRow("list-element", iceCream.name, iceCream.price + "$");
+    listElements.appendChild(getRow("list-element", iceCream.name, iceCream.price + "$"));
 })
+list.appendChild(listElements);
